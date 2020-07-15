58a92ef827e031ac0fe8fb2a63195bc97b757142
Andreas Maierhofer <a.maierhofer@gmail.com>
2020-07-15 10:45:29 +0200

    Insert license headers

--- test/tarantula/tarantula_test.rb
+++ test/tarantula/tarantula_test.rb
@@ -1,5 +1,11 @@
 # encoding: utf-8
 
+#  Copyright (c) 2012-2020, CVP Schweiz. This file is part of
+#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_cvp.
+
+
 require 'test_helper'
 require 'relevance/tarantula'
 require 'tarantula/tarantula_config'
