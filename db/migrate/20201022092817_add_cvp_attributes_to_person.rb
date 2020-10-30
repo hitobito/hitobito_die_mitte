155ecf91f4eed2883d5189411ff61249cbd365cd
Nils Rauch <rauch@puzzle.ch>
2020-10-30 13:56:55 +0100

    Add reset_column_information to fix specs on travis

--- db/migrate/20201022092817_add_cvp_attributes_to_person.rb
+++ db/migrate/20201022092817_add_cvp_attributes_to_person.rb
@@ -5,5 +5,6 @@ class AddCvpAttributesToPerson < ActiveRecord::Migration[6.0]
     add_column :people, :salutation, :string, null: true
     add_column :people, :correspondence_language, :string, null: false, default: 'de'
     add_column :people, :civil_status, :string, null: false, default: 'single'
+    Person.reset_column_information
   end
 end
