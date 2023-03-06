015365e9f95c4c7d3bb45b66bbda58fa7ad86883
Nils Rauch <rauch@puzzle.ch>
2023-03-06 15:03:44 +0100

    Remove buggy performance optimization

---
+++ app/domain/die_mitte/abilities/person_readables.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2023, Die Mitte. This file is part of
+#  hitobito_die_mitte and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_die_mitte.
+
+module DieMitte::PersonReadables
+
+  def initialize(user, group = nil, roles_join = nil)
+    super(user, group, roles_join)
+
+    can :index, Person, accessible_people { |_| true }
+  end
+
+end
