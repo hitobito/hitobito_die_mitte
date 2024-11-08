81bdf59009e322f131ad2c0d8d820dcdb78be5d8
Matthias Viehweger <viehweger@puzzle.ch>
2024-11-08 16:10:27 +0100

    Search for searchable attrs of this wagon

--- spec/domain/search_strategies/person_search_spec.rb
+++ spec/domain/search_strategies/person_search_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2021, Die Mitte. This file is part of
+#  Copyright (c) 2021-2024, Die Mitte. This file is part of
 #  hitobito_die_mitte and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_die_mitte.
@@ -11,32 +11,11 @@ describe SearchStrategies::PersonSearch do
   describe "#search_fulltext" do
     let(:user) { people(:admin) }
 
-    it "finds accessible person by title" do
-      result = search_class(people(:admin).title.to_s).search_fulltext
+    it "finds accessible person by role-label" do
+      search_term = 'Admingott'
 
-      expect(result).to include(people(:admin))
-    end
-
-    it "finds accessible person by website" do
-      result = search_class(people(:admin).website.to_s).search_fulltext
-
-      expect(result).to include(people(:admin))
-    end
-
-    it "finds accessible person by correspondence_language" do
-      result = search_class(people(:admin).correspondence_language.to_s).search_fulltext
-
-      expect(result).to include(people(:admin))
-    end
-
-    it "finds accessible person by civil_status" do
-      result = search_class(people(:admin).civil_status.to_s).search_fulltext
-
-      expect(result).to include(people(:admin))
-    end
-
-    it "finds accessible person by salutation" do
-      result = search_class(people(:admin).salutation.to_s).search_fulltext
+      user.roles << Group::BundSekretariat::ItSupport.new(group: groups(:sekretariat), label: search_term)
+      result = search_class(search_term).search_fulltext
 
       expect(result).to include(people(:admin))
     end
